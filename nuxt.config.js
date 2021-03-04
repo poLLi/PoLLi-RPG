496a3abca853dfed2330e7c4b4eaf7c2da9738ef
PoLLi <1657076+poLLi@users.noreply.github.com>
2021-03-04 11:17:50 +0100

    Nuxt Setup

---
+++ nuxt.config.js
@@ -0,0 +1,58 @@
+export default {
+    // Global page headers: https://go.nuxtjs.dev/config-head
+    head: {
+        title: 'polli-rpg',
+        meta: [
+            { charset: 'utf-8' },
+            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+            { name: 'author', content: 'Tim Pollerhof' },
+            { hid: 'description', name: 'description', content: 'Roleplaying Game based on my Real-Life' }
+        ],
+        link: [
+            { rel: 'icon', type: 'image/x-icon', href: '/favicon/favicon.ico' },
+            { rel: 'icon', type: 'image/png', href: '/favicon/android-icon-192x192.png', sizes: '192x192' },
+            { rel: 'apple-touch-icon', href: '/favicon/apple-icon-180x180.png', sizes: '180x180' }
+        ]
+    },
+
+    // Global Loading Animation
+    loading: false,
+
+    // Global CSS: https://go.nuxtjs.dev/config-css
+    css: ['~/assets/scss/main.scss'],
+
+    // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+    plugins: [],
+
+    // Auto import components: https://go.nuxtjs.dev/config-components
+    components: true,
+
+    // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+    buildModules: [],
+
+    // Modules: https://go.nuxtjs.dev/config-modules
+    modules: [
+        // https://go.nuxtjs.dev/axios
+        '@nuxtjs/axios',
+        // https://go.nuxtjs.dev/pwa
+        '@nuxtjs/pwa'
+    ],
+
+    // Axios module configuration: https://go.nuxtjs.dev/config-axios
+    axios: {
+        baseURL: 'http://127.0.0.1:1337'
+        // baseURL: 'http://127.0.0.1:1337'
+    },
+
+    // PWA module configuration: https://go.nuxtjs.dev/pwa
+    pwa: {
+        manifest: {
+            lang: 'en'
+        }
+    },
+
+    // Build Configuration: https://go.nuxtjs.dev/config-build
+    build: {
+        extractCSS: true
+    }
+};
